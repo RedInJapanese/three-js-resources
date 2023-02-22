777dda898bb7f94296f29e18d2a3f2a98e9567f3
RedInJapanese <abasu924@gmail.com>
2023-02-22 10:43:30 -0800

    update: script.js(lesson_06)

--- lesson_06/src/script.js
+++ lesson_06/src/script.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three'
+import { MeshDepthMaterial } from 'three'
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
@@ -27,5 +28,43 @@ scene.add(camera)
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
+
+
 renderer.setSize(sizes.width, sizes.height)
-renderer.render(scene, camera)
+
+//animations
+const clock = new THREE.Clock()
+var x = 0
+var time = Date.now()
+function tick(){
+
+    const current_time = Date.now()
+    const delta_time = current_time-time
+    time = current_time
+    console.log(delta_time)
+
+    const elapsed = clock.getElapsedTime()
+    if(mesh.position.x >= 5){
+        x+=1
+        if(x%2 != 0){
+            mesh.material.color.set('blue')
+        }
+        else{
+            mesh.material.color.set('red')
+        }
+        mesh.position.x = -5
+    }
+    else{
+        mesh.position.x += 0.001*delta_time
+    }
+    mesh.rotation.y+=0.001*delta_time
+    mesh.rotation.z+=0.001*delta_time
+    mesh.position.y = Math.sin(elapsed)
+    camera.position.x = Math.sin(elapsed)
+    camera.position.y = Math.cos(elapsed)
+    console.log('tic')
+    renderer.render(scene, camera)
+    window.requestAnimationFrame(tick) //passes in the tick function as an argument
+}
+
+tick() 
